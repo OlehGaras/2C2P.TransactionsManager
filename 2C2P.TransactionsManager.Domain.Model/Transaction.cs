b32e28f458bf3cfba8a874869efce852fab78db7
Oleh Harasymchuk <olehgaras@gmail.com>
2020-02-29 14:15:34 +0200

    [2c2p-01] Added domain models

---
+++ 2C2P.TransactionsManager.Domain.Model/Transaction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _2C2P.TransactionsManager.Domain.Model
+{
+    public class Transaction
+    {
+        public string TransactionId { get; set; }
+        public decimal Amount { get; set; }
+        public string CurrencyCode { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public TransactionStatus Status { get; set; }
+    }
+}
