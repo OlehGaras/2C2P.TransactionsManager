bbfbaba2c3dffd49f017dd762e33fba9bc6b84fe
Oleh Harasymchuk <olehgaras@gmail.com>
2020-02-29 14:27:22 +0200

    [2c2p-01] Added empty asp .net core  web app

---
+++ 2C2P.TransactionsManager.Web/Controllers/UploadTransactionFileController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _2C2P.TransactionsManager.Controllers
+{
+    public class UploadTransactionFileController : Controller
+    {
+        public UploadTransactionFileController()
+        {
+        }
+
+        public IActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Upload(IFormFile file)
+        {
+
+            await Task.CompletedTask;
+            return View();
+        }
+    }
+}
---
+++ 2C2P.TransactionsManager.Web/Startup.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace _2C2P.TransactionsManager.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler(builder =>
+                    builder.Run(async context =>
+                        {
+                            context.Response.StatusCode = 500;
+                            await context.Response.WriteAsync("An unexpected fault happened. Try again later.");
+                        }
+                    ));
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=UploadTransactionFile}/{action=Upload}");
+            });
+        }
+    }
+}
